ddff891478581062d01355d76da2f5d8dc5e5d31
Nikita Home <lupikisnikita@gmail.com>
2017-03-05 10:44:13 +0200

    added admin crud for post,user. Some other things

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -2,20 +2,62 @@
 
 namespace app\controllers;
 
+use Yii;
 use app\models\Post;
+use app\models\PostSearch;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
 
+/**
+ * PostController implements the CRUD actions for Post model.
+ */
 class PostController extends Controller
 {
-    public function actionView($id)//todo pagination
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
     {
-
-        $Post = Post::find()
-            ->where(['id' => $id])
-            ->limit(1)
-            ->one();
-
-        return $this->render('post', ['Post' => $Post]);
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
     }
 
+
+    /**
+     * Displays a single Post model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+
+
+
+    /**
+     * Finds the Post model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Post the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Post::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
 }
--- models/Post.php
+++ models/Post.php
@@ -2,35 +2,96 @@
 
 namespace app\models;
 
-
+use Yii;
 use yii\db\ActiveRecord;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\SluggableBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
 
-class Post extends ActiveRecord
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $slug
+ * @property string $lead_photo
+ * @property string $lead_text
+ * @property string $content
+ * @property string $meta_description
+ * @property string $date_created
+ * @property string $date_modified
+ * @property integer $created_by
+ * @property integer $updated_by
+ * @property integer $category_id
+ */
+class Post extends \yii\db\ActiveRecord
 {
 
-//    private $id;
-//    public $content;
-//    public $dateCreated;
-//    public $dateModified;
 
-
-    public function rules()
+    public function behaviors()
     {
         return [
-            // the name, email, subject and body attributes are required
-            [[ 'title' ,'content', 'date_created', 'date_modified'], 'required']
-
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['date_created', 'date_modified'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['date_created'],
+                ],
+                 'value' => new Expression('NOW()'),
+            ],
+            [
+                'class' => SluggableBehavior::className(),
+                'attribute' => 'title',
+                'slugAttribute' => 'slug',
+            ],
         ];
     }
 
     /**
-    * @return string the name of the table associated with this ActiveRecord class.
-    */
+     * @inheritdoc
+     */
     public static function tableName()
     {
-        return '{{post}}';
+        return 'post';
     }
 
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'slug', 'content' , 'created_by', 'category_id'], 'required'],
+            [['lead_text', 'content'], 'string'],
+//            [['date_created', 'date_modified'], 'safe'],
+            [['created_by', 'updated_by', 'category_id'], 'integer'],
+            [['title', 'slug', 'lead_photo'], 'string', 'max' => 128],
+            [['meta_description'], 'string', 'max' => 160],
+            [['title'], 'unique'],
+            [['slug'], 'unique'],
+        ];
+    }
 
-
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'slug' => 'Slug',
+            'lead_photo' => 'Lead Photo',
+            'lead_text' => 'Lead Text',
+            'content' => 'Content',
+            'meta_description' => 'Meta Description',
+            'date_created' => 'Date Created',
+            'date_modified' => 'Date Modified',
+            'created_by' => 'Created By',
+            'updated_by' => 'Updated By',
+            'category_id' => 'Category ID',
+        ];
+    }
 }
